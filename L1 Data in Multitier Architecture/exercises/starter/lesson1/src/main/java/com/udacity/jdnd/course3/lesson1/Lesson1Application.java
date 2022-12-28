1b3f8444427ab36292f17e1f0ca62e2dbcc8eff9
Obinna Ogbonna <obinnaogbonnajoseph@gmail.com>
2022-12-28 12:58:35 +0100

    data-store solutions

--- L1 Data in Multitier Architecture/exercises/starter/lesson1/src/main/java/com/udacity/jdnd/course3/lesson1/Lesson1Application.java
+++ L1 Data in Multitier Architecture/exercises/starter/lesson1/src/main/java/com/udacity/jdnd/course3/lesson1/Lesson1Application.java
@@ -2,8 +2,10 @@ package com.udacity.jdnd.course3.lesson1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class Lesson1Application {
 
 	public static void main(String[] args) {
